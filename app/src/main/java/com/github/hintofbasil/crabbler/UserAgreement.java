206404b5ca3409c018d62ade6358350e8c244922
William Hutcheson <hintofbasil@github.com>
2016-05-02 19:15:44 +0100

    Application closes on declining user agreement

--- app/src/main/java/com/github/hintofbasil/crabbler/UserAgreement.java
+++ app/src/main/java/com/github/hintofbasil/crabbler/UserAgreement.java
@@ -3,11 +3,15 @@ package com.github.hintofbasil.crabbler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class UserAgreement extends AppCompatActivity {
 
     TextView agreement;
+    Button acceptButton;
+    Button declineButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,15 @@ public class UserAgreement extends AppCompatActivity {
 
         agreement = (TextView) findViewById(R.id.agreement_text);
         agreement.setMovementMethod(new ScrollingMovementMethod());
+
+        acceptButton = (Button) findViewById(R.id.accept_button);
+        declineButton = (Button) findViewById(R.id.decline_button);
+
+        declineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }

c4ecd9c1dffcec94d4095ece889d8ebf0991e0c8
William Hutcheson <hintofbasil@github.com>
2016-05-02 20:05:38 +0100

    Renamed UserAgreement to UserAgreementActivity

--- app/src/main/java/com/github/hintofbasil/crabbler/UserAgreementActivity.java
+++ app/src/main/java/com/github/hintofbasil/crabbler/UserAgreementActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class UserAgreement extends AppCompatActivity {
+public class UserAgreementActivity extends AppCompatActivity {
 
     TextView agreement;
     Button acceptButton;

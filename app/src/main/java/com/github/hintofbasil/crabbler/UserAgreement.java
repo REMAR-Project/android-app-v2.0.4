fc5cfab0f20475bf88e2814c39a373d00c8fcedd
William Hutcheson <hintofbasil@github.com>
2016-05-02 17:01:02 +0100

    Initilised application

---
+++ app/src/main/java/com/github/hintofbasil/crabbler/UserAgreement.java
@@ -0,0 +1,13 @@
+package com.github.hintofbasil.crabbler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserAgreement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_agreement);
+    }
+}

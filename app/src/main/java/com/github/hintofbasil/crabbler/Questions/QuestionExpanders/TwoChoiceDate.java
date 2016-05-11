5c230d7632e73a621f8c6437914a7ecaf5139ad4
hintofbasil <hintofbasil@github.com>
2016-05-11 12:31:20 +0100

    Added choice saving and loading to TwoChoiceDate

--- app/src/main/java/com/github/hintofbasil/crabbler/Questions/QuestionExpanders/TwoChoiceDate.java
+++ app/src/main/java/com/github/hintofbasil/crabbler/Questions/QuestionExpanders/TwoChoiceDate.java
@@ -1,6 +1,8 @@
 package com.github.hintofbasil.crabbler.Questions.QuestionExpanders;
 
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,6 +14,9 @@ import org.json.JSONObject;
 
 public class TwoChoiceDate extends Expander {
 
+    CheckBox choiceOneCheckBox;
+    CheckBox choiceTwoCheckBox;
+
     public TwoChoiceDate(AppCompatActivity activity) {
         super(activity);
     }
@@ -22,22 +27,52 @@ public class TwoChoiceDate extends Expander {
 
         ImageView imageView = (ImageView) activity.findViewById(R.id.image);
         TextView titleView = (TextView) activity.findViewById(R.id.title);
-        CheckBox choiceOneCheckBox = (CheckBox) activity.findViewById(R.id.choice_one);
-        CheckBox choiceTwoCheckBox = (CheckBox) activity.findViewById(R.id.choice_two);
+        choiceOneCheckBox = (CheckBox) activity.findViewById(R.id.choice_one);
+        choiceTwoCheckBox = (CheckBox) activity.findViewById(R.id.choice_two);
 
         imageView.setImageDrawable(getDrawable(question.getString("questionPicture")));
         titleView.setText(question.getString("questionTitle"));
         choiceOneCheckBox.setText(question.getString("choiceOneText"));
         choiceTwoCheckBox.setText(question.getString("choiceTwoText"));
+
+        choiceOneCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choiceTwoCheckBox.setChecked(false);
+            }
+        });
+
+        choiceTwoCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choiceOneCheckBox.setChecked(false);
+            }
+        });
     }
 
     @Override
     protected void setPreviousAnswer(String answer) {
-
+        switch(answer) {
+            case "0":
+                choiceOneCheckBox.setChecked(true);
+                break;
+            case "1":
+                choiceTwoCheckBox.setChecked(true);
+                break;
+            default:
+                Log.d("TwoChoiceDate", "No previous answer");
+                break;
+        }
     }
 
     @Override
     public String getSelectedAnswer() {
-        return null;
+        if(choiceOneCheckBox.isChecked()) {
+            return "0";
+        } else if(choiceTwoCheckBox.isChecked()) {
+            return "1";
+        } else {
+            return null;
+        }
     }
 }

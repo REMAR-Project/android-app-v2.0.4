1721fcf8fb80374abe5405d98c8d1ae38ee02a5d
hintofbasil <hintofbasil@github.com>
2016-05-17 18:06:50 +0100

    Added multiple date selecting

--- app/src/main/java/com/github/hintofbasil/crabbler/Questions/QuestionExpanders/DateRange.java
+++ app/src/main/java/com/github/hintofbasil/crabbler/Questions/QuestionExpanders/DateRange.java
@@ -3,16 +3,19 @@ package com.github.hintofbasil.crabbler.Questions.QuestionExpanders;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.github.hintofbasil.crabbler.R;
 import com.roomorama.caldroid.CaldroidFragment;
+import com.roomorama.caldroid.CaldroidListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by will on 14/05/16.
@@ -33,12 +36,24 @@ public class DateRange extends Expander {
         imageView.setImageDrawable(getDrawable(question.getString("questionPicture")));
         titleView.setText(question.getString("questionTitle"));
 
+        // Build calendar
+
+        CaldroidListener caldroidListener = new CaldroidListener() {
+            @Override
+            public void onSelectDate(Date date, View view) {
+                view.setBackgroundResource(R.color.questionPreviouslySelectedBackground);;
+            }
+        };
+
         CaldroidFragment caldroidFragment = new CaldroidFragment();
         Bundle args = new Bundle();
         Calendar cal = Calendar.getInstance();
         args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
         args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        args.putBoolean(CaldroidFragment.ENABLE_SWIPE, false);
+        args.putBoolean(CaldroidFragment.SHOW_NAVIGATION_ARROWS, false);
         caldroidFragment.setArguments(args);
+        caldroidFragment.setCaldroidListener(caldroidListener);
 
         FragmentTransaction t = activity.getSupportFragmentManager().beginTransaction();
         t.replace(R.id.content_layout, caldroidFragment);

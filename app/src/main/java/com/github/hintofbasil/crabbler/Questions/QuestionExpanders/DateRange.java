a7bf8bcef5a5fc25c2934b90b39d05550bc23e15
hintofbasil <hintofbasil@github.com>
2016-05-17 17:38:25 +0100

    Added basic calendar to application

--- app/src/main/java/com/github/hintofbasil/crabbler/Questions/QuestionExpanders/DateRange.java
+++ app/src/main/java/com/github/hintofbasil/crabbler/Questions/QuestionExpanders/DateRange.java
@@ -1,13 +1,18 @@
 package com.github.hintofbasil.crabbler.Questions.QuestionExpanders;
 
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.github.hintofbasil.crabbler.R;
+import com.roomorama.caldroid.CaldroidFragment;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.Calendar;
 
 /**
  * Created by will on 14/05/16.
@@ -27,6 +32,17 @@ public class DateRange extends Expander {
 
         imageView.setImageDrawable(getDrawable(question.getString("questionPicture")));
         titleView.setText(question.getString("questionTitle"));
+
+        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        Bundle args = new Bundle();
+        Calendar cal = Calendar.getInstance();
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        caldroidFragment.setArguments(args);
+
+        FragmentTransaction t = activity.getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.content_layout, caldroidFragment);
+        t.commit();
     }
 
     @Override

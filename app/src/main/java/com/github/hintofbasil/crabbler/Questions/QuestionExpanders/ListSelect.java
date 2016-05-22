3153897dc0bef30f92fa51f8b79315455aa2b135
hintofbasil <hintofbasil@github.com>
2016-05-22 13:32:55 +0100

    Added basic layout for list_select

--- app/src/main/java/com/github/hintofbasil/crabbler/Questions/QuestionExpanders/ListSelect.java
+++ app/src/main/java/com/github/hintofbasil/crabbler/Questions/QuestionExpanders/ListSelect.java
@@ -1,6 +1,10 @@
 package com.github.hintofbasil.crabbler.Questions.QuestionExpanders;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.github.hintofbasil.crabbler.R;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,6 +20,13 @@ public class ListSelect extends Expander {
 
     @Override
     public void expandLayout(JSONObject question) throws JSONException {
+        activity.setContentView(R.layout.activity_date_range);
+
+        ImageView imageView = (ImageView) activity.findViewById(R.id.image);
+        TextView titleView = (TextView) activity.findViewById(R.id.title);
+
+        imageView.setImageDrawable(getDrawable(question.getString("questionPicture")));
+        titleView.setText(question.getString("questionTitle"));
     }
 
     @Override

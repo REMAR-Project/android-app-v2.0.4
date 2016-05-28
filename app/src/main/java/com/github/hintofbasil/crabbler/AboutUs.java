38cb84ee482a2c1e15ef55253d07ad857011408e
hintofbasil <hintofbasil@github.com>
2016-05-28 16:51:58 +0100

    Added empty fragment for about us

--- app/src/main/java/com/github/hintofbasil/crabbler/AboutUs.java
+++ app/src/main/java/com/github/hintofbasil/crabbler/AboutUs.java
@@ -1,7 +1,15 @@
 package com.github.hintofbasil.crabbler;
 
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class AboutUs extends AppCompatActivity {
 
@@ -9,5 +17,28 @@ public class AboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        try {
+            JSONArray data = readJSON();
+            FragmentManager fm = getFragmentManager();
+            fm.beginTransaction().add(R.id.fragment_container,
+                    AboutUsFragment.newInstance("HellO!", "hhhhhhhhhh"),
+                    AboutUsFragment.FRAGMENT_TAG)
+            .commit();
+            /*for(int i=0;i<data.length();i++) {
+                JSONObject dataObject = data.getJSONObject(0);
+            }*/
+        } catch (IOException|JSONException e) {
+            Log.e("AboutUs", "Unable to read about_us.json\n" + Log.getStackTraceString(e));
+            return;
+        }
+    }
+
+    public JSONArray readJSON() throws IOException, JSONException {
+        InputStream jsonInputStream = getBaseContext().getResources().openRawResource(R.raw.about_us);
+        byte[] buffer = new byte[8192];
+        int length = jsonInputStream.read(buffer);
+        String jsonString = new String(buffer).substring(0, length);
+        return new JSONArray(jsonString);
     }
 }
---
+++ app/src/main/java/com/github/hintofbasil/crabbler/AboutUsFragment.java
@@ -0,0 +1,98 @@
+package com.github.hintofbasil.crabbler;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AboutUsFragment extends Fragment {
+
+    private static final String ARG_TITLE = "TITLE_ARG";
+    private static final String ARG_CONTENT = "CONTENT_ARG";
+    public static final String FRAGMENT_TAG = "ABOUT_US_FRAGMENT_TAG";
+
+    private String title;
+    private String content;
+
+    private OnFragmentInteractionListener mListener;
+
+    public AboutUsFragment() {
+
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param title Parameter 1.
+     * @param content Parameter 2.
+     * @return A new instance of fragment AboutUsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AboutUsFragment newInstance(String title, String content) {
+        AboutUsFragment fragment = new AboutUsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_TITLE, title);
+        args.putString(ARG_CONTENT, content);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            title = getArguments().getString(ARG_TITLE);
+            content = getArguments().getString(ARG_CONTENT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_us, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}

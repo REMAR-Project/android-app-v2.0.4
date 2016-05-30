36a761f1a2282c19bb8af6ff262f0c90e907cca0
hintofbasil <hintofbasil@github.com>
2016-05-30 13:05:12 +0100

    Updated all keys to be public static final

--- app/src/main/java/com/github/hintofbasil/crabbler/Keys.java
+++ app/src/main/java/com/github/hintofbasil/crabbler/Keys.java
@@ -5,14 +5,14 @@ package com.github.hintofbasil.crabbler;
  */
 public class Keys {
 
-    public static String SETTINGS_PREFS_KEY = "SETTINGS_PREFS_KEY";
-    public static String AGREEMENT_ACCEPTED_KEY = "AGREEMENT_ACCEPTED_KEY";
-    public static String TO_SEND_PREFERENCES_KEY = "TO_SEND_PREFERENCES_KEY";
-    public static String SAVED_PREFERENCES_KEY = "SAVED_PREFERENCES_KEY";
-    public static String QUESTION_ID_KEY = "QUESTION_ID_KEY";
-    public static String ANSWERS_KEY = "ANSWERS_KEY";
-    public static String PHONE_ID_KEY = "PHONE_ID_KEY";
-    public static String ACCESS_TOKEN_KEY = "ACCESS_TOKEN_KEY";
+    public static final String SETTINGS_PREFS_KEY = "SETTINGS_PREFS_KEY";
+    public static final String AGREEMENT_ACCEPTED_KEY = "AGREEMENT_ACCEPTED_KEY";
+    public static final String TO_SEND_PREFERENCES_KEY = "TO_SEND_PREFERENCES_KEY";
+    public static final String SAVED_PREFERENCES_KEY = "SAVED_PREFERENCES_KEY";
+    public static final String QUESTION_ID_KEY = "QUESTION_ID_KEY";
+    public static final String ANSWERS_KEY = "ANSWERS_KEY";
+    public static final String PHONE_ID_KEY = "PHONE_ID_KEY";
+    public static final String ACCESS_TOKEN_KEY = "ACCESS_TOKEN_KEY";
 
     public static final String REGISTER = "REGISTER";
     public static final String LOGIN = "LOGIN";

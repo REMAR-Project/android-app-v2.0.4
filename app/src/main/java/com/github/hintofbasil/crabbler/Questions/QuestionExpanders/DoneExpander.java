702817192cea60b5ec83d35266524a2d5285a652
hintofbasil <hintofbasil@github.com>
2016-06-05 16:42:03 +0100

    Added question submitting to done

--- app/src/main/java/com/github/hintofbasil/crabbler/Questions/QuestionExpanders/DoneExpander.java
+++ app/src/main/java/com/github/hintofbasil/crabbler/Questions/QuestionExpanders/DoneExpander.java
@@ -1,11 +1,15 @@
 package com.github.hintofbasil.crabbler.Questions.QuestionExpanders;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
+import com.github.hintofbasil.crabbler.BackgroundDataPost.DataPostFactory;
+import com.github.hintofbasil.crabbler.Keys;
+import com.github.hintofbasil.crabbler.Questions.QuestionActivity;
 import com.github.hintofbasil.crabbler.R;
 
 import org.json.JSONException;
@@ -23,6 +27,16 @@ public class DoneExpander extends Expander {
     @Override
     public void expandLayout(JSONObject question) throws JSONException {
         activity.setContentView(R.layout.expander_done);
+
+        CheckBox done = (CheckBox) activity.findViewById(R.id.chk_done);
+        done.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked)
+                    return;
+                postAnswers();
+            }
+        });
     }
 
     @Override
@@ -33,5 +47,29 @@ public class DoneExpander extends Expander {
     @Override
     public String getSelectedAnswer() {
         return null;
+    }
+
+    private void postAnswers() {
+        DataPostFactory dataPostFactory = new DataPostFactory(activity.getBaseContext());
+        String answers = popAnswers();
+        // Turns semi-colons into commas for processing
+        answers = answers.replaceAll(";", ",");
+        dataPostFactory.submitAnswers(answers);
+
+        Toast.makeText(activity.getBaseContext(),
+                activity.getString(R.string.thank_you_toast),
+                Toast.LENGTH_LONG).show();
+
+        Intent intent = new Intent(activity.getBaseContext(),
+                QuestionActivity.class);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    private String popAnswers() {
+        this.prefs = activity.getSharedPreferences(Keys.SAVED_PREFERENCES_KEY, Context.MODE_PRIVATE);
+        String answers = prefs.getString(Keys.ANSWERS_KEY, null);
+        prefs.edit().remove(Keys.ANSWERS_KEY).remove(Keys.QUESTION_ID_KEY).apply();
+        return answers;
     }
 }
